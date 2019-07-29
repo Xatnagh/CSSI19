954df3bc5baee020fb55aba369de3690b76631d9
hello world <darrenzou1221@gmail.com>
2019-07-29 14:40:21 -0400

    hi

---
+++ day5/math.js
@@ -0,0 +1,6 @@
+function f(x){
+    return 3*x+7;
+}
+console.log(f(5));
+
+var t = Math.sqrt(40);
--- day5/script.js
+++ day5/script.js
@@ -3,3 +3,16 @@ var i;
 for(i=1;i<=10;i++){
     console.log(i);
 }
+let result = prompt("enter your name");
+console.log("hello", result);
+let num = prompt("how many dankness?");
+let oj = Number(num);
+console.log(oj+3000);
+
+var grade = prompt("enter your grade to determine your place in the corporate world");
+var gradenum= Number(grade);
+if(!isNaN(grade)&& gradenum<=70){
+console.log("you passed");
+}else{
+    console.log("you failed");
+}
