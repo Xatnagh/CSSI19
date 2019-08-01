ae6436d6a0c586293f9b072dbfa9effb8d7c7ccc
hello world <darrenzou1221@gmail.com>
2019-08-01 16:40:55 -0400

    python basics

---
+++ day7/mystory.py
@@ -0,0 +1,39 @@
+story = """Baby shark, doo doo doo doo doo doo
+Baby shark, doo doo doo doo doo doo
+Baby shark, doo doo doo doo doo doo
+Baby shark!
+Mommy shark, doo doo doo doo doo doo
+Mommy shark, doo doo doo doo doo doo
+Mommy shark, doo doo doo doo doo doo
+Mommy shark!
+Daddy shark, doo doo doo doo doo doo
+Daddy shark, doo doo doo doo doo doo
+Daddy shark, doo doo doo doo doo doo
+Daddy shark!
+{0} shark, doo doo doo doo doo doo
+Grandma shark, doo doo doo doo doo doo
+Grandma shark, doo doo doo doo doo doo
+Grandma shark!
+Grandpa shark, doo doo doo doo doo doo
+Grandpa shark, doo doo doo doo doo doo
+Grandpa shark, doo doo {1} doo doo doo
+Grandpa shark!
+Let's {2} hunt, doo doo doo doo doo doo
+Let's go hunt, doo doo doo doo doo doo
+Let's go hunt, doo doo doo doo doo doo
+Let's go hunt!
+"""
+noun=raw_input("Enter a noun \n")
+doodoo = raw_input("Enter doodoo \n")
+verb = raw_input("Enter a verb \n")
+print story.format(noun,doodoo,verb)
+
+
+def f():
+    print 'o'
+    def r():
+        print "i"
+    r()
+
+f()
+
